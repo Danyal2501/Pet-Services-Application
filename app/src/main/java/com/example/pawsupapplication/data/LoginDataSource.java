37fca39b92e9ef167a4be1fd74542f523bad948e
Wader <waderzheng12345@gmail.com>
2021-10-07 23:35:07 -0400

    DREAM-37-Established login authentication using mock database.

--- app/src/main/java/com/example/pawsupapplication/data/LoginDataSource.java
+++ app/src/main/java/com/example/pawsupapplication/data/LoginDataSource.java
@@ -4,20 +4,36 @@ import com.example.pawsupapplication.data.model.LoggedInUser;
 
 import java.io.IOException;
 
+import java.util.HashMap;
+
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
  */
 public class LoginDataSource {
 
-    public Result<LoggedInUser> login(String username, String password) {
+    HashMap<String, String> testData = new HashMap<>();
 
+    public Result<LoggedInUser> login(String username, String password) {
+        LoggedInUser currentUser;
+        testData.put("Testbot1", "Aa1234567");
+        testData.put("Testbot2", "1234567Aa");
+        testData.put("Testbot3", "556677YYY");
+        testData.put("Testbot4", "!aGoodTime1");
+        testData.put("Testbot5", "YBHI567411!");
+        boolean pass = false;
         try {
             // TODO: handle loggedInUser authentication
-            LoggedInUser fakeUser =
-                    new LoggedInUser(
-                            java.util.UUID.randomUUID().toString(),
-                            "Jane Doe");
-            return new Result.Success<>(fakeUser);
+            if(testData.get(username) != null && testData.get(username).equals(password)) {
+                pass = true;
+            }
+            if(pass) {
+                currentUser =
+                        new LoggedInUser(
+                                java.util.UUID.randomUUID().toString(),
+                                " " + username);
+                return new Result.Success<>(currentUser);
+            }
+            return null;
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
         }
