99f74ba827dc9ff14a7d0b671ff5977b6d0e6159
AnnasRahuma <62153944+AnnasRahuma@users.noreply.github.com>
2021-11-03 19:00:22 -0400

    Dream-77 subtask for DREAM-63 Cleaning up PetCards Pages Cleaned up data base for petCards and added DAO

--- app/src/main/java/com/example/pawsupapplication/ui/petcard/PetCards.java
+++ app/src/main/java/com/example/pawsupapplication/ui/petcard/PetCards.java
@@ -5,8 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.pawsupapplication.R;
+import com.example.pawsupapplication.data.DAO;
 import com.example.pawsupapplication.data.adapter.PetCardAdapter;
 
 import java.util.ArrayList;
@@ -24,10 +26,17 @@ public class PetCards extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pet_cards);
+        System.out.println("2");
         Intent i = getIntent();
-        ArrayList<String> arr =  getIntent().getStringArrayListExtra("map");
-        ArrayList<String> arr2 =  getIntent().getStringArrayListExtra("map1");
-        PetCardAdapter a = new PetCardAdapter(this, arr.toArray(), arr2.toArray());
+        System.out.println("3");
+       // ArrayList<String> arr =  getIntent().getStringArrayListExtra("map");
+        DAO dbh = new DAO(PetCards.this);
+        System.out.println("4");
+        ArrayList<String> arr3 = dbh.getPetsInfo();
+        ArrayList<String> arr4 = dbh.getPetsPic();
+       // Toast.makeText(this, arr3.toString(), Toast.LENGTH_LONG).show();
+        //ArrayList<String> arr2 =  getIntent().getStringArrayListExtra("map1");
+        PetCardAdapter a = new PetCardAdapter(this, arr3.toArray(), arr4.toArray());
         ListView cardList = (ListView)findViewById(R.id.petLister);
         cardList.setAdapter(a);
 
