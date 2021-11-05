bedd799f74331ab7ea8e15fdd1dd2ca50a26410f
danya <danyalrana2010@gmail.com>
2021-11-05 05:48:19 -0400

    Added reset password feature, email is being sent to proper address if the email is registered. TODO: Set new password in database, based on what the user inputs.

--- app/src/main/java/com/example/pawsupapplication/ui/services/ServiceDetails.java
+++ app/src/main/java/com/example/pawsupapplication/ui/services/ServiceDetails.java
@@ -1,5 +1,6 @@
 package com.example.pawsupapplication.ui.services;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -7,6 +8,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -23,6 +25,7 @@ public class ServiceDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servicedetails);
     }
+    PopupWindow popupWindow;
     public void onButtonShowPopupWindowClick(View view) {
 
         // inflate the layout of the popup window
@@ -34,7 +37,7 @@ public class ServiceDetails extends AppCompatActivity {
         int width = LinearLayout.LayoutParams.WRAP_CONTENT;
         int height = LinearLayout.LayoutParams.WRAP_CONTENT;
         boolean focusable = true; // lets taps outside the popup also dismiss it
-        final PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
+        popupWindow = new PopupWindow(popupView, width, height, focusable);
 
         // show the popup window
         // which view you pass in doesn't matter, it is only used for the window tolken
@@ -48,5 +51,17 @@ public class ServiceDetails extends AppCompatActivity {
                 return true;
             }
         });
+    }
+    //Give toast and return to services if yes
+    public void deleteService(View view){
+        Toast.makeText(getApplicationContext(), "Service Deleted", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(this, ServiceActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    //dismiss popup if user presses no
+    public void noDeleteService(View view){
+        popupWindow.dismiss();
     }
 }
