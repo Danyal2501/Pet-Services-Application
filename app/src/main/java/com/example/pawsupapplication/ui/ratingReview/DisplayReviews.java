fd851ea04a67a81d03d84d759e6c342fed9cbee0
Shu Sun <shu.sun@mail.utoronto.ca>
2021-11-05 08:24:39 -0400

    DREAM-18 Testing commits.

--- app/src/main/java/com/example/pawsupapplication/ui/ratingReview/DisplayReviews.java
+++ app/src/main/java/com/example/pawsupapplication/ui/ratingReview/DisplayReviews.java
@@ -24,19 +24,19 @@ public class DisplayReviews extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.displayreview_activity);
-        ArrayList<String> arr =  getIntent().getStringArrayListExtra("map");
-        ArrayList<String> arr2 =  getIntent().getStringArrayListExtra("map1");
+        ArrayList<String> arr = getIntent().getStringArrayListExtra("map");
+        ArrayList<String> arr2 = getIntent().getStringArrayListExtra("map1");
         ReviewAdapter a = new ReviewAdapter(this, arr.toArray(), arr2.toArray());
-        ListView ReviewList = (ListView)findViewById(R.id.reviewLister);
+        ListView ReviewList = (ListView) findViewById(R.id.reviewLister);
         ReviewList.setAdapter(a);
     }
 
-    public void add(View v){
+    public void add(View v) {
         Intent i = new Intent(this, ReviewActivity.class);
         startActivity(i);
     }
 
-    public void back(View v){
+    public void back(View v) {
         Intent i = new Intent(this, RecentServices.class);
         startActivity(i);
     }
